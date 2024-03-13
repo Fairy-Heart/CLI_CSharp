610e3a6e49843f52ba7b838ec2017a2c905264e0
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-03-13 14:56:31 +0700

    Add main file

---
+++ main/main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using command;
+
+
+namespace fav 
+{
+    public class MainApplication 
+    {
+        public static void Main(string[] args)
+        {
+
+
+            SystemInfomation SystemObject = new SystemInfomation();
+
+            if(args.Length > 0)            
+            {
+                switch(args[0])
+                {
+                    case "--systeminfo":
+                    SystemObject.SystemInfo();
+                    break;
+
+                }
+            }
+            
+            if(args.Length == 0)
+            {
+               Console.WriteLine("Try fav --help to show all command");
+            }
+        }
+    }
+}
