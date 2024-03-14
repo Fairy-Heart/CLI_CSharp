0d3c702ca355f7c20b79d6f87336fa31e292d999
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-03-15 01:07:36 +0700

    Update file os_check.cs

---
+++ main/README.md
@@ -0,0 +1,6 @@
+*Hello, this is a command-line application written similarly to Bash, and will have more features added in the future. You can request pulls for new features.*
+
+**Text Editor I use:**  
+# Visual Studio Code
+
+# .NET Version: 9.0
--- main/command/os_check.cs
+++ main/command/os_check.cs
@@ -1,8 +1,58 @@
+using System.Runtime.InteropServices;
+
 namespace os_check 
 {
     public class os_checked
     {
-        public bool isWindows = OperatingSystem.IsWindows();
-        public bool isLinux = OperatingSystem.IsLinux();
+        public static bool Sys_Check()
+        {
+            // bool isWindows = 
+            // bool isMacOs = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+            // bool isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+            // bool isBDS = RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
+
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Your System is : ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" [WINDOWS]\n");
+                Console.ResetColor();
+                return true;
+            } else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Your System is : ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" [MAC OS]\n");
+                Console.ResetColor();
+                return true;
+            } else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Your System is : ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" [LINUX]\n");
+                Console.ResetColor();
+                return true;
+            } else if(RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD)) {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("Your System is : ");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" [FREE BSD]\n");
+                Console.ResetColor();
+                return true;
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Your System Is : Undefine Operating System\n");
+                Console.ResetColor();
+            }
+            return false;
+        }
     }
 }
